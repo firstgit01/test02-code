5b5ff052ceda2b6fd558e5f63f3e579724416060
user <neetamalik07@gmail.com>
2021-03-21 17:48:40 +0530

    i am pushing all my eatlier files from code folder

---
+++ first.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    cout<<"HELLO SIR ......\n What can i do for you\n commend me"<<endl;
+    char a[1000];
+    gets(a);
+    cout<<"do you wwant to see the content???"<<endl;
+    puts(a);
+    cout<<"Good Bye\n";
+    return 0;
+}
